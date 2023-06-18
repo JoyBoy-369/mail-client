42244dd300fc09ba2fc63feb4d9f77e9a54e0fa0
kevin n <kevin.nin92@gmail.com>
2023-06-18 15:09:13 +0530

    feat: instrumentation

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::net::TcpListener;
 
 use mail_client::configuration::get_configuration;
 use mail_client::startup::run;
+use mail_client::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("mail-client".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     // Panic if we can't read config
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection = PgPool::connect(&configuration.database.connection_string())
